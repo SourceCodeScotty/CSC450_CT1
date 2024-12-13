47e7c6394317fd613612e3578639a8199756133d
SourceCodeScotty <scottmillerfc@gmail.com>
2024-12-12 22:43:43 -0500

    Add files via upload

---
+++ Corrections_2/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+//#include<conio.h> (not a valid mac os comand)
+//Standard namespace declaration (comment)
+using namespace std;
+//Main Function (comment)
+int main()
+{
+    //this should be printed out (I assume at the end and not here as well)
+    double myMoney = 1000.50;
+
+
+    //Standard Ouput Statement (comment)
+    cout<<"Please be sure to correct all syntax errors in this program\n" // \n instead of endl
+    "I have corrected all errors for this program.\n" //remove cout to simplify
+     " The total amount of money available is " << myMoney << endl;
+
+    // Wait For Output Screen
+    //Main Function return Statement (comment)
+    return 0;
+}
