2738be953d4639fa8138b333449a8db998d114ba
SourceCodeScotty <scottmillerfc@gmail.com>
2024-12-12 22:39:54 -0500

    Add files via upload

---
+++ Corrections_1/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+// need to get rid of include<conio.h>
+// it is an older libary and is not supported on my mac OS
+
+// Standard namespace declaration (make this a comment here)
+using namespace std; //tells the complier that I am using std
+//This makes it so I don't need to write it in front of cout
+
+// Main Function (make this a comment here)
+int main() {
+    //Standard Ouput Statement
+    cout<<"Welcome to this C++ Program\n" //add a new line
+    "I have corrected all errors for this program."; //remove cout as it is redundant
+    return 0; //Return 0 instead od end
+}
